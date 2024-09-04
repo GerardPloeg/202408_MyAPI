6b5639dd4e2aa3804c7f6ea6e29de82625cb8925
Gerard Ploeg <PgJ@hoornbeeck.nl>
2024-09-04 09:47:38 +0200

    Seeder added

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use app\Models\Book;
 
 class BookSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 25; $i++)
+        {
+            Book::create(['name' => $faker->sentence,
+                          'author' => $faker->name,
+                          'price' => $faker->randomFloat(2, 0, 100)]);
+        }
     }
 }
